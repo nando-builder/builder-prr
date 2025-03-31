ffce4500e4bd677c129e5ba70840cc0e3d944373
Fernando Duran <fernando@builder.io>
2025-03-31 12:57:41 +0100

    adding webhook to update cache in vercel - slug dynv2

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,17 +1,16 @@
-// pages/api/revalidate.ts
-
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Check for the secret in headers instead of the query string
   const secret = req.headers['x-vercel-secret'];
 
+  // Validate the secret
   if (secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid secret' });
   }
 
   try {
-    const { slug } = req.body;
+    // Get slug from query parameter
+    const slug = req.query.slug as string;
 
     if (!slug) {
       return res.status(400).json({ message: 'Slug is required' });
